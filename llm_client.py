51c104e34c7a75a51d75c9fe524510ef39c387d0
ivvlko <ivankoevbg@gmail.com>
2025-05-27 22:54:43 +0300

    add error handling and handle 401 from server

--- llm_client.py
+++ llm_client.py
@@ -27,16 +27,24 @@ def chat():
     
     clean_enabled = input("Do you want to hide the thought process behind model' decision? ").strip().lower() in ["yes", "y"]
 
-    while True:
-        user_input = input("You: ").strip()
-        if user_input.lower() == "exit":
-            print("Goodbye!")
-            break
+    try:
+        while True:
+            user_input = input("You: ").strip()
+            if user_input.lower() == "exit":
+                print("Goodbye!")
+                break
 
-        response = send_request(client, user_input)
-        if clean_enabled:
-            response = clean_thought_process_from_response(response)
-        print("Assistant:", response)
+            response = send_request(client, user_input)
+
+            if "Error: 401" in response:
+                raise Exception("unauthorized")
+
+            if clean_enabled:
+                response = clean_thought_process_from_response(response)
+
+            print("Assistant:", response)
+    except Exception as e:
+            print(f"oops, something went wrong, check message: {str(e)}")
 
 if __name__ == "__main__":
     chat()
